6b6669209f7b245516c91f82779ac77061366e07
Marcin Wijas <marcinw1577@gmail.com>
2024-01-23 20:23:42 +0100

    Removing comma, change data type columns

--- amazon_products_marcin.sql
+++ amazon_products_marcin.sql
@@ -1,10 +1,3 @@
--- All data 
-select * from public.amazon_products ap;
-
-
-select * from public.duplicate_products dp; 
-
-
 -- Create copy table 
 create table duplicate_products as (select * from amazon_products ap);
 
@@ -31,21 +24,70 @@ update duplicate_products set discount_price = replace(discount_price, '₹', ''
 update duplicate_products set actual_price = replace(actual_price, '₹', '');  
 
 
-----------
+-------------------------------------------------------
 
 
-select * from public.duplicate_products dp; 
+-- Cast column ratings to float type and ignoring cells
+alter table duplicate_products
+alter column ratings
+type float
+using nullif(ratings, '')::float;
 
 
+-- Removing char ₹ from discount_price column
+update duplicate_products 
+set discount_price = trim(discount_price, '₹');
+
+-- Removing comma from discount_price column
+update duplicate_products 
+set discount_price = regexp_replace(discount_price, ',', '', 'g'); 
 
 
-
+-- Cast column discount_price to float type and ignoring cells
 alter table duplicate_products 
-alter column ratings type varchar using ratings::numeric,
-alter column no_of_ratings type varchar using no_of_ratings::integer,     
-alter column discount_price type varchar using discount_price::numeric,   
-alter column actual_price type varchar using actual_price::numeric;   
+alter column discount_price
+type float
+using nullif(discount_price, '')::float;
 
+
+-- Removing char ₹ from actual_price
+update duplicate_products 
+set actual_price = trim(actual_price, '₹');
+
+
+-- Removing comma from actual_price
+update duplicate_products
+set actual_price = regexp_replace(actual_price, ',', '', 'g');
+
+
+-- Cast column actual_price to float type and ignoring cells
+alter table duplicate_products 
+alter column actual_price
+type float
+using nullif(actual_price, '')::float;
+
+
+-- Removing comma from no_of_ratings column
+update duplicate_products 
+set no_of_ratings  = regexp_replace(no_of_ratings, ',', '', 'g');
+
+
+-- Cast column no_of_ratings to numeric type and ignore blank cells
+alter table duplicate_products 
+alter column no_of_ratings
+type numeric
+using nullif(no_of_ratings, '')::numeric;
+
+
+----------------------------------------------
+
+/* 
+analyze catogories:
+ 1 accessories, 
+ 3 bags & luggage, 
+ 6 grocery & gourmet foods, 
+ 17 toys & baby products 
+*/
 
 
 
