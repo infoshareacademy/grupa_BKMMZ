e76ac710d4950e528f580230f44dd1d43b5b1990
Marcin Wijas <marcinw1577@gmail.com>
2024-01-23 10:27:11 +0100

    adds alter table, alter column, correction needed

--- amazon_products_marcin.sql
+++ amazon_products_marcin.sql
@@ -9,24 +9,19 @@ select * from public.duplicate_products dp;
 create table duplicate_products as (select * from amazon_products ap);
 
 
--- Clear data 'FREE' on ratings
+-- Clear records 'FREE' on ratings
 delete from duplicate_products dp 
 where dp.ratings = 'FREE';
 
 
--- Clear data 'Get' on ratings
+-- Clear records 'Get' on ratings
 delete from duplicate_products dp 
 where dp.ratings = 'Get';
 
 
--- Clear data where '₹' appear in ratings column
+-- Clear records where '₹' appear in ratings column
 delete from duplicate_products dp 
 where dp.ratings like '₹%';
-
-
--- Clear data where '₹' appear in discount_price column
-delete from duplicate_products dp 
-where  like '₹%';
 
 
 -- Delete '₹' from discount_price records.
@@ -36,12 +31,20 @@ update duplicate_products set discount_price = replace(discount_price, '₹', ''
 update duplicate_products set actual_price = replace(actual_price, '₹', '');  
 
 
-select actual_price, count(*) 
-from duplicate_products dp
-group by actual_price;
+----------
+
+
+select * from public.duplicate_products dp; 
 
 
 
+
+
+alter table duplicate_products 
+alter column ratings type varchar using ratings::numeric,
+alter column no_of_ratings type varchar using no_of_ratings::integer,     
+alter column discount_price type varchar using discount_price::numeric,   
+alter column actual_price type varchar using actual_price::numeric;   
 
 
 
