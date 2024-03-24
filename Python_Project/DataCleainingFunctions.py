401dde9e78c9b849475ab011108b23b6246e96d7
CrisMagdziak <162913111+CrisMagdziak@users.noreply.github.com>
2024-03-24 09:04:31 +0100

    Project

--- Python_Project/DataCleainingFunctions.py
+++ Python_Project/DataCleainingFunctions.py
@@ -1,4 +1,5 @@
 def stats(dataframe) :
+    """Returns informations about dataset - shape and data types"""
     print('### Shape ###')
     print(f'Rows: {dataframe.shape[0]}')
     print(f'Columns: {dataframe.shape[1]}')
@@ -6,8 +7,20 @@ def stats(dataframe) :
     print('### Data Types ###')
     print(dataframe.dtypes)
     print('\n')
+
+def miss_dupl(dataframe) :
+    """Return information about missing values and duplicates in dataset"""
+    print('\n')
     print('### Missing Values ###')
     print(dataframe.isna().sum())
     print('\n')
     print('### Duplicate Values ###')
     print(dataframe.duplicated().sum())
+    print('\n')
+    print('### Unique Values ###')
+    print(dataframe.nunique().sum())
+
+def drop_treshold(dataframe) :
+    """ Return infrmation which column is in drop treshold of 5% missing values"""
+    tresh = int(len(dataframe) * 0.05)
+    return dataframe.columns[dataframe.isna().sum() <= tresh]
