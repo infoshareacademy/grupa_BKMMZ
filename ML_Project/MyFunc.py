9452a1dbb2b5fbe4b64c1f828b0218cad603225c
CrisMagdziak <162913111+CrisMagdziak@users.noreply.github.com>
2024-06-11 21:04:14 +0200

    Modeling

--- ML_Project/MyFunc.py
+++ ML_Project/MyFunc.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 def classify_row(value):
     """ Function that check if or contains word Dwelling
 
@@ -11,3 +14,90 @@ def classify_row(value):
         return 'Business '
     else:
         return 'Non-Business'
+    
+
+def dataset_IQR(col) :
+    """ Calculate IQR for given column
+     
+    Args:
+        col -> column for IQR calculation
+
+    Return:
+        IQR value of specific column, value -> float
+    
+    """
+    return np.quantile(col, 0.75) - np.quantile(col, 0.25)
+
+def lower_tresh(col):
+    """ Calculate lower treshold
+
+    Args:
+        col -> column for IQR calculation
+
+    Return:
+        lower outliner value -> float
+    """ 
+    return np.quantile(col, 0.25) - (1.5 * dataset_IQR(col))
+
+def upper_tresh(col) :
+    """ Calculate upper treshold
+
+    Args:
+        col -> column for IQR calculation
+
+    Return:
+        upper outliner value -> float
+    """ 
+    return np.quantile(col, 0.75) + (1.5 * dataset_IQR(col))
+
+def lower_treshholders(dataframe) :
+    """ Check how many outliners-lower we have for each column
+    
+    Args:
+        dataframe -> dataframe from where we load data
+    
+    """
+    for x in dataframe.select_dtypes(include= 'number') :
+        if dataframe[x][dataframe[x] < lower_tresh(dataframe[x])].count() > 0 :
+            print(f'Column: {x}')
+            print(f'Lower treshold {lower_tresh(dataframe[x])}')
+            print(f'Ilosc wartosci ponizej dolnego outlinera: {dataframe[x][dataframe[x] < lower_tresh(dataframe[x])].count()}\n')
+        
+
+def upper_treshholders(dataframe) :
+    """ Check how many outliners-upper we have for each column
+    
+    Args:
+        dataframe -> dataframe from where we load data
+    
+    """
+    for x in dataframe.select_dtypes(include= 'number') :
+        if dataframe[x][dataframe[x] > upper_tresh(dataframe[x])].count() > 0 :
+            print(f'Column: {x}')
+            print(f'Upper treshold {upper_tresh(dataframe[x])}')
+            print(f'Ilosc wartosci powyzej górnego outlinera: {dataframe[x][dataframe[x] > upper_tresh(dataframe[x])].count()}\n')
+
+
+def is_numeric(col):
+    """ Check if columns have only numeric values
+
+        Args: 
+            col -> columns where we are checking
+    """
+    try:
+        pd.to_numeric(col)
+        return True
+    except ValueError:
+        return False
+    
+
+def encode_labels(df, list, encoder) :
+    """ Function that encode columns to label
+
+    Args: 
+        list -> type List, columns that need to be encoded
+    return dataframe
+    """
+    for col in list:
+        df[col] = encoder.fit_transform(df[col])
+    return df
